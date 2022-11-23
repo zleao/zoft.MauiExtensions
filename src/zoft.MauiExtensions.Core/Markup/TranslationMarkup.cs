3d0fbdd6d2b6baf27f8ad4d2f46ed62570fee954
José Pereira <leaopereira@gmail.com>
2022-11-23 11:33:18 +0100

    Move ServicHelper to a separate class

--- src/zoft.MauiExtensions.Core/Markup/TranslationMarkup.cs
+++ src/zoft.MauiExtensions.Core/Markup/TranslationMarkup.cs
@@ -24,22 +24,4 @@ namespace zoft.MauiExtensions.Core.Markup
             return ProvideValue(serviceProvider);
         }
     }
-
-
-    public static class ServiceHelper
-    {
-
-        public static T GetService<T>() => Current.GetService<T>();
-
-        public static IServiceProvider Current =>
-#if WINDOWS10_0_17763_0_OR_GREATER
-    MauiWinUIApplication.Current.Services;
-#elif ANDROID
-    MauiApplication.Current.Services;
-#elif IOS || MACCATALYST
-    MauiUIApplicationDelegate.Current.Services;
-#else
-            null;
-#endif 
-    }
 }
---
+++ src/zoft.MauiExtensions.Core/Services/ServiceHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace zoft.MauiExtensions.Core.Services
+{
+    public static class ServiceHelper
+    {
+
+        public static T GetService<T>() => Current.GetService<T>();
+
+        public static IServiceProvider Current =>
+#if WINDOWS10_0_17763_0_OR_GREATER
+            MauiWinUIApplication.Current.Services;
+#elif ANDROID
+            MauiApplication.Current.Services;
+#elif IOS || MACCATALYST
+            MauiUIApplicationDelegate.Current.Services;
+#else
+            throw new NotSupportedException("ServiceProvider not supported in current platform");
+#endif 
+    }
+}
