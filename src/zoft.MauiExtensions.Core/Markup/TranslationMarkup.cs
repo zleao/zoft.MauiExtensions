a54d4c01be8346fb7f79f8dd2b79775bfe54ce56
José Pereira <leaopereira@gmail.com>
2022-11-24 16:42:04 +0100

    Remove Warning message on release build, by adding missing documentation

--- src/zoft.MauiExtensions.Core/Markup/TranslationMarkup.cs
+++ src/zoft.MauiExtensions.Core/Markup/TranslationMarkup.cs
@@ -2,11 +2,27 @@
 
 namespace zoft.MauiExtensions.Core.Markup
 {
+    /// <summary>
+    /// Extensions Markup that provides a transaltion service, bsed on the <see cref="ILocalizationService"/>
+    /// </summary>
     [ContentProperty(nameof(Name))]
     public sealed class Translate : IMarkupExtension<BindingBase>
     {
+        /// <summary>
+        /// Name of the key to get from the text resource source
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Converter to apply to the bound value
+        /// </summary>
         public IValueConverter Converter { get; set; }
+
+        /// <summary>
+        /// Provides the BindingBase instance to apply
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        /// <returns></returns>
 
         public BindingBase ProvideValue(IServiceProvider serviceProvider)
         {
--- src/zoft.MauiExtensions.Core/Services/Implementations/MainThreadService.cs
+++ src/zoft.MauiExtensions.Core/Services/Implementations/MainThreadService.cs
@@ -1,19 +1,28 @@
 ﻿namespace zoft.MauiExtensions.Core.Services
 {
+    /// <inheritdoc/>
     public sealed class MainThreadService : IMainThreadService
     {
+
+        /// <inheritdoc/>
         public bool IsMainThread => MainThread.IsMainThread;
 
+        /// <inheritdoc/>
         public void BeginInvokeOnMainThread(Action action) => MainThread.BeginInvokeOnMainThread(action);
 
+        /// <inheritdoc/>
         public Task<SynchronizationContext> GetMainThreadSynchronizationContextAsync() => MainThread.GetMainThreadSynchronizationContextAsync();
 
+        /// <inheritdoc/>
         public Task InvokeOnMainThreadAsync(Action action) => MainThread.InvokeOnMainThreadAsync(action);
 
+        /// <inheritdoc/>
         public Task<T> InvokeOnMainThreadAsync<T>(Func<T> func) => MainThread.InvokeOnMainThreadAsync(func);
 
+        /// <inheritdoc/>
         public Task InvokeOnMainThreadAsync(Func<Task> funcTask) => MainThread.InvokeOnMainThreadAsync(funcTask);
 
+        /// <inheritdoc/>
         public Task<T> InvokeOnMainThreadAsync<T>(Func<Task<T>> funcTask) => MainThread.InvokeOnMainThreadAsync(funcTask);
     }
 }
